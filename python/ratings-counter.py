73d8bc7b3b23d6fdd51f3b7a0872eea950b55a57
Seok Won <ikr@kakao.com>
2021-01-06 22:29:38 +0900

    Over python 3.7+ dict is OrderedDict by default
    I don't have to use OrderedDict from collections

--- python/ratings-counter.py
+++ python/ratings-counter.py
@@ -1,15 +1,13 @@
 from pyspark.sql import SparkSession
-from collections import OrderedDict
 
 # Spark v3.0.1
 spark = SparkSession.builder.master("local").appName("RatingsHistogram").getOrCreate()
 
 lines = spark.read.option("header", True).csv("./ml-latest-small/ratings.csv")
 ratings = lines.rdd.map(lambda line: line[2])  # line is already list
-result = ratings.countByValue()
+result = dict(sorted(ratings.countByValue().items()))
 
-sortedResult = OrderedDict(sorted(result.items()))
-for key, value in sortedResult.items():
+for key, value in result.items():
     print(f"{key}, {value}")
 
 spark.stop()
