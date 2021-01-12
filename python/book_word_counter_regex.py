eb0ceaec066444271accdff796780f5c4ad02dfe
Seok Won <ikr@kakao.com>
2021-01-12 22:58:49 +0900

    Create txt word counter with regex
    using python's re

--- python/book_word_counter_regex.py
+++ python/book_word_counter_regex.py
@@ -1,12 +1,18 @@
+import re as regex
+
 from pyspark.sql import SparkSession
 
 
+def normalizeWords(text):
+    return regex.compile(r"\W+", regex.UNICODE).split(text)
+
+
 # Spark v3.0.1
-spark = SparkSession.builder.master("local").appName("WordCounter").getOrCreate()
+spark = SparkSession.builder.master("local").appName("WordCounter2").getOrCreate()
 spark.sparkContext.setLogLevel("ERROR")
 
 lines = spark.sparkContext.textFile("./data/DaddyLongLeg_book_korean.txt")
-words = lines.flatMap(lambda words: words.split())  # line is already list
+words = lines.flatMap(normalizeWords)
 wordCounts = words.countByValue()
 
 # mostFreqWord = max(wordCounts, key=lambda x: wordCounts[x])
